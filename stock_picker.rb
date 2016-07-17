a857d2285871e2c4f31e64cc5a0c922c93ea4858
schleiwalker17 <luke.j.schleicher@gmail.com>
2016-07-17 00:12:49 -0500

    whitespace correction

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,21 +3,17 @@ def stock_picker(array)
 	best_days = []
 	biggest_gain = 0
 
-	# what I need to do is make a rule that says
-	# that a buy must be before a sell
-
 	array[0..-2].each_with_index do |first_price, index|
 		array[(index+1)..-1].each_with_index do |second_price, index|
 
-				diff = first_price - second_price
+			diff = first_price - second_price
 
-				if (biggest_gain > diff)
-					buy_day = array.index(first_price)
-					sell_day = array.index(second_price)
-					best_days = [buy_day,sell_day]
-					biggest_gain = diff
-				end
-
+			if (biggest_gain > diff)
+				buy_day = array.index(first_price)
+				sell_day = array.index(second_price)
+				best_days = [buy_day,sell_day]
+				biggest_gain = diff
+			end
 		end
 	end
 
